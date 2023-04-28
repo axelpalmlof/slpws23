e3e36510754c889e0e9512bb408e11071d5e1e16
axelpalmlof <axelpalmlof@gmail.com>
2023-04-28 20:37:34 +0200

    Yardoc
    Yardoc och dokumentation

--- app.rb
+++ app.rb
@@ -9,15 +9,22 @@ enable :sessions
 
 include Model
 
+  # Displays the homepage
+  #
   get('/')  do
     slim(:start)
   end 
 
+  # Displays a list of all reviews
+  #
+  # @see Model#getReview
   get('/review') do
     result = getReview()
     slim(:"review/index",locals:{review:result})
   end
 
+  # Displays a form for adding a new review, but only if the user is logged in
+  #
   get('/review/new') do
     if session[:loggedId] != nil
       slim(:"review/new")
@@ -26,47 +33,96 @@ include Model
     end
   end
   
+  # Adds a new review to the database, then redirects to the reviews index page
+  #
+  # @param [Integer] userId, the author of the review
+  # @param [String] title, the movies title
+  # @param [Sting] rating, thoughts of the movie
+  # @param [String] director, the movies director
+  #
+  # @see Model#newReview
   post('/review/new') do
     newReview()
     redirect('/review')
   end
 
+  # Updates an existing review in the database, then redirects to the reviews index page
+  #
+  # @param [Integer] id, the reviews id
+  # @param [Integer] userId, the author of the review
+  # @param [String] title, the movies title
+  # @param [Sting] rating, thoughts of the movie
+  # @param [String] director, the movies director
+  #
+  # @see Model#updateReview
   post('/review/:id/update') do
     updateReview()
     redirect('/review')
   end
   
+  # Displays a form for editing an existing review, but only if the user is logged in and is the author of the review
+  #
+  # @param [Integer] id, the reviews id
+  #
+  # @see Model#editAuth
+  # @see Model#editReview
   get('/review/:id/edit') do
-    #editAuth()
-    result = editReview()
-    slim(:"/review/edit",locals:{result:result})
+    auth = editAuth()
+    if auth == true
+      result = editReview()
+      slim(:"/review/edit",locals:{result:result})
+    else
+      redirect('/review')
+    end
   end
 
+  # Deletes an existing review from the database, but only if the user is logged in and is the author of the review
+  #
+  # @param [Integer] id, the reviews id
+  #
+  # @see Model#deleteAuth
   post('/review/:id/delete') do
     deleteAuth()
   end
 
+  # Displays the login form
+  #
   get('/login') do
     slim(:"/login")
   end
 
+  # Authenticates the user and logs them in
+  #
+  # @param [String] username, The username
+  # @param [String] password, The password
+  #
+  # @see Model#loginUser
   post('/login') do
     loginUser()
   end
 
+  # Displays a form for creating a new user account
+  # 
   get('/user/new') do
     slim(:"user/new")
   end
 
+  # Creates a new user account in the database, then logs the user in and redirects to the homepage
+  #
+  # @see Model#newUser
   post('/user/new') do
     newUser()
   end
 
+  # Logs the user out by clearing the session data, then redirects to the login page
+  #
   get('/logout') do
       session.clear
-      redirect('/')
+      redirect('/login')
   end
 
+  # Displays form to add a movie you want to watch
+  #
   get('/movie/new') do
     if session[:loggedId]
       slim(:'movie/new')
@@ -75,11 +131,20 @@ include Model
     end
   end
 
+  # Creates a movie in the database that a user wants to see
+  #
+  # @param [Integer] userId, the author of the review
+  # @param [String] title, the movies title
+  #
+  # @see Model#newMovie
   post('/movie/new') do
     newMovie()
     redirect('/movie')
   end
 
+  # Shows movies that the logged in user wants to se
+  #
+  # @see Model#getMovie
   get('/movie') do
     if session[:loggedId]
       result = getMovie()
@@ -89,6 +154,12 @@ include Model
     end
   end
 
+  # Deletes a movie from the watch list
+  #
+  # @param [Integer] movie_id, the movie that is gettig deleted
+  # @param [Integer] user_id, the user that had that movie in watchlist
+  #
+  # @see Model#deleteMovie
   post('/movie/:id/delete/') do
     if session[:loggedId]
       deleteMovie()      
@@ -98,6 +169,11 @@ include Model
     end
   end
 
+  # Shows a review someone has wrote
+  #
+  # @param [Integer] id, the reviews id
+  #
+  # @see Model#showReview
   get('/review/:id') do
     result = showReview()
     slim(:'review/show', locals: { review: result })
--- model.rb
+++ model.rb
@@ -1,20 +1,35 @@
 module Model
 
+    # Connect to database with read and write access
+    #
+    # @return [Database]
     def dbConnect()
         db = SQLite3::Database.new("db/db.db")
         db.results_as_hash = true
         return db
     end
 
+    # Connect to database with write access only
+    #
+    # @return [Database]
     def dbConnectWh()
         return SQLite3::Database.new("db/db.db")
     end
     
+    # Retrieve all reviews from database
+    #
+    # @return [Hash]
     def getReview()
         db = dbConnect()
         return db.execute("SELECT * FROM review")
     end
 
+    # Add a new review to database
+    #
+    # @param [Integer] userId, the author of the review
+    # @option params [String] title, the movies title
+    # @option params [Sting] rating, thoughts of the movie
+    # @option params [String] director, the movies director
     def newReview()
         userId = session[:loggedId]
         title = params[:title]
@@ -24,6 +39,13 @@ module Model
         db.execute("INSERT INTO review (userId, title, rating, director) VALUES (?,?,?,?)",userId, title, rating, director)
     end
 
+    # Update an existing review in database
+    #
+    # @param [Integer] id, the reviews id
+    # @param [Integer] userId, the author of the review
+    # @param [String] title, the movies title
+    # @option params [Sting] rating, thoughts of the movie
+    # @param [String] director, the movies director
     def updateReview()
         id = params[:id].to_i
         title = params[:title]
@@ -34,17 +56,30 @@ module Model
         db.execute("UPDATE review SET title=?,userId=?,rating=?,director=? WHERE reviewId =?",title,userId,rating,director,id)
     end
 
+    # Retrieve a specific review from database
+    #
+    # @param [Integer] reviewId, the reviews id
+    #
+    # @return [Hash]
     def showReview()
         db = dbConnect()
         return db.execute("SELECT * FROM review WHERE reviewId = ?", params[:id]).first
     end
 
+    # Retrieve a specific review for editing from database
+    #
+    # @param [Integer] reviewId, the reviews id
+    #
+    # @return [Hash]
     def editReview()
         id = params[:id].to_i
         db = dbConnect()
         return db.execute("SELECT * FROM review WHERE reviewId = ?",id).first
     end
 
+    # Delete a specific review from database
+    #
+    # @param [Integer] reviewId, the reviews id
     def deleteReview()
         slim(:"review/new")
         id = params[:id].to_i
@@ -53,6 +88,9 @@ module Model
         redirect('/review')
     end
 
+    # Check if the user is authorized to delete a specific review
+    #
+    # @param [Integer] reviewId, the reviews id
     def deleteAuth()
         id = params[:id].to_i
         db = dbConnectWh()
@@ -65,18 +103,28 @@ module Model
         end
     end
 
-    # def editAuth()
-    #     id = params[:id].to_i
-    #     db = dbConnectWh()
-    #     tempId = db.execute("SELECT userId FROM review WHERE reviewId = ?",id)[0][0]
+    # Check if the user is authorized to edit a specific review
+    #
+    # @param [Integer] reviewId, the reviews id
+    #
+    # @return [True]
+    # @return [False]
+    def editAuth()
+        id = params[:id].to_i
+        db = dbConnectWh()
+        tempId = db.execute("SELECT userId FROM review WHERE reviewId = ?",id)[0][0]
 
-    #     if session[:loggedId] == tempId || session[:loggedId] == 1
-    #         newReview()
-    #     else
-    #         redirect("/review")
-    #     end
-    # end
+        if session[:loggedId] == tempId || session[:loggedId] == 1
+            return true
+        else
+            return false
+        end
+    end
 
+    # Logs in a user with username and password
+    #
+    # @option params [String] username, The username
+    # @option params [String] password, The password
     def loginUser()
         username = params[:username]
         password = params[:password]
@@ -97,10 +145,15 @@ module Model
         end
     end
 
+    # Adds a new user to the database with a username and a password
+    #
+    # @option params [String] username, The username
+    # @option params [String] password, The password
     def newUser()
         username = params[:username]
         password = params[:password]
         passwordConfirm = params[:passwordConfirm]  
+        
         if password == passwordConfirm
           passwordDigest = BCrypt::Password.create(password)
           db = SQLite3::Database.new('db/db.db')
@@ -112,11 +165,16 @@ module Model
         end
     end
 
+    # Adds a movie into a users watchlist
+    #
+    # @param [Integer] userId, the watchlists user
+    # @option params [String] title, the movies title
     def newMovie()
         userId = session[:loggedId]
         title = params[:title]
         db = dbConnectWh()
         movieId = db.execute("SELECT movieId FROM movie WHERE title = ?", title).first
+        
         if movieId.nil?
             db.execute("INSERT INTO movie (title) VALUES (?)", title)
             movieId = db.last_insert_row_id
@@ -126,15 +184,24 @@ module Model
         db.execute("INSERT INTO user_movie (userId, movieId) VALUES (?, ?)", userId, movieId)
     end
 
+    # Shows all movies on a users watchlist
+    #
+    # @return [Hash]
     def getMovie()
         db = dbConnect()
         return db.execute("SELECT * FROM movie INNER JOIN user_movie ON movie.movieId=user_movie.movieId WHERE user_movie.userId=?", session[:loggedId])
     end
 
+    # Deletes a movie into a users watchlist
+    #
+    # @param [Integer] movie_id, the movie that is gettig deleted
+    # @param [Integer] user_id, the user that had that movie in watchlist
     def deleteMovie()
         movie_id = params[:id].to_i
         user_id = session[:loggedId] 
         db = dbConnectWh()
         db.execute("DELETE FROM user_movie WHERE userId=? AND movieId=?", user_id, movie_id)
     end
+
+
 end
